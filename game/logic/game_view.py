a7e28b6439f9342b12b20a0fc79e895364b30a6e
Tibovc <tibo.van.craenenbroeck@student.howes.be>
2020-11-09 18:45:25 +0100

    Application availible on other devices

--- game/logic/game_view.py
+++ game/logic/game_view.py
@@ -1,9 +1,17 @@
-import eel
+import eel, socket
 
 
 class GameView:
     def __init__(self):
-        eel.init("./game\web")
-        eel.start("index.html")
+        eel.init("./game/web")
+        self.start()
+        
     
+    def start(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        eel.start("index.html", port=80, host=ip)
+
+
+
 a = GameView()
