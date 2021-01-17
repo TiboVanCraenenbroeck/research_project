b4403196e6a79f3699683716fc7c9193928b7d82
Tibovc <tibo.van.craenenbroeck@student.howes.be>
2021-01-17 19:52:35 +0100

    - Added train_function

--- rl/muzero_4x4.py
+++ rl/muzero_4x4.py
@@ -1,3 +1,4 @@
+import random
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
@@ -20,6 +21,7 @@ import eel
 from datetime import datetime
 from pathlib import Path
 import pickle
+import collections
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -28,21 +30,24 @@ sys.path.insert(0, BASE_DIR)
 from game.logic.game import Game
 
 class MuZero:
-    def __init__(self, learning_rate: float):
+    def __init__(self, learning_rate: float, replay_memory_size: int, batch_size=int):
         self.learning_rate = learning_rate
 
-        self.start()
+        self.start(replay_memory_size, batch_size)
 
         self.nn_hidden_state, self.nn_prediction, self.nn_dynamic = self.build_nn()
         self.make_model()
     
     def save_model(self):
-        self.nn_hidden_state.save("h.h5")
-        self.nn_prediction.save("f.h5")
-        self.nn_dynamic.save("g.h5")
-        self.model.save("base_model.h5")
+        self.nn_hidden_state.save(f"{self.base_path}/h.h5")
+        self.nn_prediction.save(f"{self.base_path}/f.h5")
+        self.nn_dynamic.save(f"{self.base_path}/g.h5")
+        self.model.save(f"{self.base_path}/base_model.h5")
 
-    def start(self):
+    def start(self, replay_memory_size, batch_size):
+        self.agent_name = f"v_{datetime.now().strftime('%d_%m_%y__%H_%M%S')}"
+        self.base_path = f"{BASE_DIR}/start_models/a2c/{self.agent_name}"
+        Path(self.base_path).mkdir(parents=True, exist_ok=True)
         # Vars
         self.dim_hidden_state = 16
         self.dim_prediction_function_policy: int = 16
@@ -53,6 +58,10 @@ class MuZero:
 
         self.number_neurons_prediction_function: int = 16
         self.number_neurons_dynamic_function: int = 16
+
+        self.batch_size = batch_size
+
+        self.replay_memory = collections.deque(maxlen=replay_memory_size)
     
     def build_nn(self):
         # Input
@@ -130,9 +139,96 @@ class MuZero:
         self.model = Model(inputs=[input_game_grid, input_shape, input_current_action], outputs=outputs)
         self.model.compile(loss=loss, optimizer=Adam(self.learning_rate))
 
+    def train_nn(self):
+        # Sample from batch
+        if len(self.replay_memory)>= self.batch_size:
+            mini_batches = random.sample(self.replay_memory, self.batch_size)
+            observation_states = np.asarray(list(zip(*mini_batches))[0], dtype=float)
+            observation_shapes = np.asarray(list(zip(*mini_batches))[1], dtype=float)
+            actions = np.asarray(list(zip(*mini_batches))[2], dtype=float)
+            values = np.asarray(list(zip(*mini_batches))[3], dtype=float)
+            rewards = np.asarray(list(zip(*mini_batches))[4], dtype=float)
+
+            
+    
+
+    def train(self, episodes):
+        for episode in range(episodes):
+            # Get start-time
+            start_time = time.time()
+
+            # Reset the env
+            uid: str = self.env.reset()
+            state = self.env.game_env
+            state[state > 0] = 1
+            state[state <= 0] = 0
+            state = np.resize(state, (4, 4, 1))
+            shapes_queue = self.env.shapes_queue[0]
+            shapes_queue = self.create_standard_shape(shapes_queue)
+            self.env.render()
+
+            # Game stats
+            done: bool = False
+            negative_rewards: int = 0
+            total_reward_episode: float = 0
+            total_actions = []
+            nr_full_lines = 0
+            nr_actions_true: int = 0
+            nr_actions_false: int = 0
+
+            # Iterations
+            while not done:
+                action = self.compute_action(state, shapes_queue)
+                action_shape, action_row, action_col = self.number_to_arc(
+                    action)
+                reward, full_lines, state_new, done, shapes_queue_new, uid = self.env.step(
+                    self.env.shapes_queue[action_shape], action_row, action_col)
+                shapes_queue_new = self.create_standard_shape(
+                    shapes_queue_new[0])
+                state_new[state_new > 0] = 1
+                state_new[state_new <= 0] = 0
+                state_new = np.resize(state_new, (4, 4, 1))
+
+                reward, negative_rewards, done = self.reward_function(
+                    reward, full_lines, negative_rewards, done)
+
+                # Add the data to the memory
+                self.add_to_memory(state, state_new, shapes_queue,
+                                   shapes_queue_new, action, reward, done)
+                state = state_new
+                shapes_queue = shapes_queue_new
+                self.env.render()
+
+                # Stats
+                total_reward_episode += reward
+                nr_full_lines += full_lines
+                total_actions.append(action)
+                if reward > 0:
+                    nr_actions_true += 1
+                else:
+                    nr_actions_false += 1
+
+            self.train_nn()
+            self.stats["total_rewards"].append(total_reward_episode)
+            self.stats["chosen_action"].append(total_actions)
+            self.stats["nr_full_lines"].append(nr_full_lines)
+            self.stats["nr_actions_true"].append(nr_actions_true)
+            self.stats["nr_actions_false"].append(nr_actions_false)
+            if episode % 10000 == 0:
+                try:
+                    self.save_model()
+                    with open(f'{BASE_DIR}/history_data/REINFORCE/game_history_data_{self.agent_name}.pkl', 'wb') as f:
+                        pickle.dump(self.stats, f)
+                    print(
+                        f"Episode: {episode} - total reward: {total_reward_episode} | Duration: {time.time()-start_time}")
+                except:
+                    print("Saved failed!")
+
 
         
 
 
 learning_rate = 0.01
-test = MuZero(learning_rate)
+replay_memory_size = 500000
+batch_size = 2048
+test = MuZero(learning_rate, replay_memory_size, batch_size)
